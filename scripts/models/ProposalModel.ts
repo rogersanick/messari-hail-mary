f826f27f4ca94f7ddf4aa0a1591cf44753345290
Nicholas Rogers <nicholasrogers@Nicholass-MacBook-Pro.local>
2021-09-08 11:27:00 -0400

    DATA-FIX-1: Converting all BigNumbers on mongoose model to automatically convert to simple number type. ID, startBlockNumber and endBlockNumber are all acceptable values. Change based on feedback from architecture review session.

--- scripts/models/ProposalModel.ts
+++ scripts/models/ProposalModel.ts
@@ -1,55 +1,77 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { Event } from 'ethers'
 import { Schema, SchemaTypes } from 'mongoose';
-
-export function convertRecordToSchemaData({
-  id,
-  proposer,
-  targets,
-  signatures,
-  calldatas,
-  startBlock,
-  endBlock,
-  description
-}: any): GovernorAlphaProposal {
-
-  // If any of the arguments are provided, the data is malformed.
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i] === undefined) { throw new Error("The record is malformed.") }
-  }
-   
-  return {
-    id,
-    proposer,
-    // Read only arrays must be converted to type [] for mongoose
-    targets: targets.concat(),
-    signatures: signatures.concat(),
-    calldatas: calldatas.concat(),
-    startBlock: startBlock,
-    endBlock: endBlock,
-    description: description
-  };
-}
 
 export const GOVERNOR_ALPHA_PROPOSAL_MODEL_NAME = "GovernorAlphaProposal"
 
-export interface GovernorAlphaProposal {
+export interface GovernorAlphaProposalType {
   id: BigNumber,
   proposer: string,
   targets: string[],
   signatures: string[],
   calldatas: string[],
-  startBlock: BigNumber
+  startBlock: BigNumber,
   endBlock: BigNumber,
   description: string
 }
 
-export const GovernorAlphaProposalSchema = new Schema<GovernorAlphaProposal>({
-    id: { type: SchemaTypes.Mixed, required: true, unique: true },
+export interface GovernorAlphaProposalModelType {
+  id: number,
+  proposer: string,
+  targets: string[],
+  signatures: string[],
+  calldatas: string[],
+  startBlock: number,
+  endBlock: number,
+  description: string
+}
+
+export class GovernorAlphaProposal implements GovernorAlphaProposalModelType {
+
+  id: number;
+  proposer: string;
+  targets: string[];
+  signatures: string[];∂
+  calldatas: string[];
+  startBlock: number;
+  endBlock: number;
+  description: string;
+
+  constructor(event: Event) {
+    const args = event.args! as any;
+    const {
+      id,
+      proposer,
+      targets,
+      signatures,
+      calldatas,
+      startBlock,
+      endBlock,
+      description
+    }: GovernorAlphaProposalType = args
+    console.log(id.toNumber());
+    console.log(startBlock.toNumber());
+    console.log(endBlock.toNumber());
+    console.log(id.toNumber());
+    this.id = id.toNumber()
+    this.proposer = proposer
+    this.targets = targets.concat()
+    this.signatures = signatures.concat()
+    this.calldatas = calldatas.concat()
+    this.startBlock = startBlock.toNumber()
+    this.endBlock = endBlock.toNumber()
+    this.description = description
+  }
+
+}
+
+export const GovernorAlphaProposalSchema = new Schema<GovernorAlphaProposalModelType>({
+    id: { type: Number, required: true, unique: true },
     proposer: { type: String, required: true },
     targets: { type: [SchemaTypes.String], required: true },
     signatures: { type: [String], required: true },
     calldatas: { type: [String], required: true },
-    startBlock: { type: SchemaTypes.Mixed, required: true },
-    endBlock: { type: SchemaTypes.Mixed, required: true },
+    startBlock: { type: Number, required: true },
+    endBlock: { type: Number, required: true },
     description: { type: String, required: true }
 });
