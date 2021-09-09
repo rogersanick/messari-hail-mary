1959b391d9133c8e259705dfb119864e3fb29446
Nicholas Rogers <nicholasrogers@Nicholass-MacBook-Pro.local>
2021-09-09 13:41:43 -0400

    BUGFIX: Updating DBI URI format

--- scripts/database.ts
+++ scripts/database.ts
@@ -5,7 +5,6 @@ import mongoose from 'mongoose';
 export default async function initializeClient(): Promise<typeof mongoose> {
   // Initialize and connect the client
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const dbName = '/messari';
   const uri: string = process.env.DB_URI!;
-  return await mongoose.connect(uri + dbName);
+  return await mongoose.connect(uri);
 }
