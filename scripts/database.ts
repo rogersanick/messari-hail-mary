3e8d69cce6d08c34dcf9f29ff910387267730035
Nicholas Rogers <nicholasrogers@Nicholass-MacBook-Pro.local>
2021-09-09 11:38:44 -0400

    NOTICK: Updating readme with new architecture. Code cleanup.

--- scripts/database.ts
+++ scripts/database.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import mongoose from 'mongoose';
 
 // Async function that returns an initialized mongo client using mongoose
 export default async function initializeClient(): Promise<typeof mongoose> {
     // Initialize and connect the client
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const dbName = "/messari"
     const uri: string = process.env.DB_URI!;
-    return await mongoose.connect(uri)
+    return await mongoose.connect(uri + dbName)
 }
